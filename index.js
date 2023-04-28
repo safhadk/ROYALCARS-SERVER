3f06ddf60646c74e6cf1d1071bbc398343a7a0ba
safhadk <safadkothikkal0880@gmail.com>
2023-04-28 16:25:20 +0530

    local host changed

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-      origin: "http://localhost:2000",
+      origin: "https://main.d1ce9rptpg52fa.amplifyapp.com",
       methods: ["GET", "POST"],
     },
   });
