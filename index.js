a7281912473b86e90a4b1f5393eacf3e662b2913
safhadk <safadkothikkal0880@gmail.com>
2024-01-04 11:37:34 +0530

    cors updated

--- index.js
+++ index.js
@@ -18,12 +18,12 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
+app.options('*', (req, res) => {
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', '*');
+  res.status(200).send();
+});
 
-// app.options('*', (req, res) => {
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-//   res.header('Access-Control-Allow-Headers', '*');
-//   res.status(200).send();
-// });
 
 
 
